139d190a1496e6948d45ecac4821896ec143caf6
Malav Shah <mshah0686@gmail.com>
2019-10-03 16:07:04 -0700

    added stuff

--- Lab_3.runs/impl_1/rundef.js
+++ Lab_3.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log segment_display.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source segment_display.tcl -notrace" );
 
--- Lab_3.runs/impl_1/segment_display.tcl
+++ Lab_3.runs/impl_1/segment_display.tcl
@@ -61,92 +61,24 @@ proc step_failed { step } {
 }
 
 
-start_step init_design
-set ACTIVE_STEP init_design
+start_step write_bitstream
+set ACTIVE_STEP write_bitstream
 set rc [catch {
-  create_msg_db init_design.pb
+  create_msg_db write_bitstream.pb
   set_param chipscope.maxJobs 1
-  create_project -in_memory -part xc7a35tcpg236-1
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
+  open_checkpoint segment_display_routed.dcp
   set_property webtalk.parent_dir C:/Users/Malav/Documents/EE316Lab/LabWork/Lab_3/Lab_3.cache/wt [current_project]
-  set_property parent.project_path C:/Users/Malav/Documents/EE316Lab/LabWork/Lab_3/Lab_3.xpr [current_project]
-  set_property ip_output_repo C:/Users/Malav/Documents/EE316Lab/LabWork/Lab_3/Lab_3.cache/ip [current_project]
-  set_property ip_cache_permissions {read write} [current_project]
-  add_files -quiet C:/Users/Malav/Documents/EE316Lab/LabWork/Lab_3/Lab_3.runs/synth_1/segment_display.dcp
-  read_xdc C:/Users/Malav/Documents/EE316Lab/LabWork/Basys3_Master.xdc
-  link_design -top segment_display -part xc7a35tcpg236-1
-  close_msg_db -file init_design.pb
+  catch { write_mem_info -force segment_display.mmi }
+  write_bitstream -force segment_display.bit 
+  catch {write_debug_probes -quiet -force segment_display}
+  catch {file copy -force segment_display.ltx debug_nets.ltx}
+  close_msg_db -file write_bitstream.pb
 } RESULT]
 if {$rc} {
-  step_failed init_design
+  step_failed write_bitstream
   return -code error $RESULT
 } else {
-  end_step init_design
-  unset ACTIVE_STEP 
-}
-
-start_step opt_design
-set ACTIVE_STEP opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force segment_display_opt.dcp
-  create_report "impl_1_opt_report_drc_0" "report_drc -file segment_display_drc_opted.rpt -pb segment_display_drc_opted.pb -rpx segment_display_drc_opted.rpx"
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step place_design
-set ACTIVE_STEP place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
-    implement_debug_core 
-  } 
-  place_design 
-  write_checkpoint -force segment_display_placed.dcp
-  create_report "impl_1_place_report_io_0" "report_io -file segment_display_io_placed.rpt"
-  create_report "impl_1_place_report_utilization_0" "report_utilization -file segment_display_utilization_placed.rpt -pb segment_display_utilization_placed.pb"
-  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file segment_display_control_sets_placed.rpt"
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-  unset ACTIVE_STEP 
-}
-
-start_step route_design
-set ACTIVE_STEP route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force segment_display_routed.dcp
-  create_report "impl_1_route_report_drc_0" "report_drc -file segment_display_drc_routed.rpt -pb segment_display_drc_routed.pb -rpx segment_display_drc_routed.rpx"
-  create_report "impl_1_route_report_methodology_0" "report_methodology -file segment_display_methodology_drc_routed.rpt -pb segment_display_methodology_drc_routed.pb -rpx segment_display_methodology_drc_routed.rpx"
-  create_report "impl_1_route_report_power_0" "report_power -file segment_display_power_routed.rpt -pb segment_display_power_summary_routed.pb -rpx segment_display_power_routed.rpx"
-  create_report "impl_1_route_report_route_status_0" "report_route_status -file segment_display_route_status.rpt -pb segment_display_route_status.pb"
-  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file segment_display_timing_summary_routed.rpt -pb segment_display_timing_summary_routed.pb -rpx segment_display_timing_summary_routed.rpx -warn_on_violation "
-  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file segment_display_incremental_reuse_routed.rpt"
-  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file segment_display_clock_utilization_routed.rpt"
-  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file segment_display_bus_skew_routed.rpt -pb segment_display_bus_skew_routed.pb -rpx segment_display_bus_skew_routed.rpx"
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  write_checkpoint -force segment_display_routed_error.dcp
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
+  end_step write_bitstream
   unset ACTIVE_STEP 
 }
 
